21ad14c979662e97eee9fc4b2a3cc513b5a3de83
Mayke <mayke19972016@gmail.com>
2019-11-08 01:35:14 -0300

    Apenas tela, e ajustes no CSS;

---
+++ SIAB/src/br/com/siab/controller/CadastroFichaAController.java
@@ -0,0 +1,11 @@
+package br.com.siab.controller;
+
+import com.jfoenix.controls.JFXTabPane;
+import javafx.fxml.FXML;
+
+public class CadastroFichaAController {
+
+    @FXML
+    private JFXTabPane tabPane;
+
+}
